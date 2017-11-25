78cb59909227d2b40bb9297792b411c47b7af577
Berk Emre Saribas <berksaribas@sabanciuniv.edu>
2017-11-26 01:30:11 +0300

    initial commit

---
+++ fixer.js
@@ -0,0 +1,19 @@
+var body = document.getElementsByTagName("BODY")[0]; 
+
+function fixList() {
+	var items = document.getElementById("items");
+	var currentIndex = parseInt(document.getElementById("publisher-container").getElementsByTagName("span")[0].innerHTML.split(" / ")[0]);
+	var maxElements = parseInt(document.getElementById("publisher-container").getElementsByTagName("span")[0].innerHTML.split(" / ")[1]);
+		
+	if(maxElements > 200 && currentIndex > 100) {
+		items.scrollTo(0, items.scrollHeight / 2);
+	}
+}
+
+body.addEventListener("yt-navigate-finish", function(event) {
+	setTimeout(function(){
+		fixList();
+	}, 100);
+});
+
+
